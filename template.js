b2d0f450f743db6c42ebf2637ce13841138130be
Kacper Wójtowicz <kacper.m.wojtowicz@gmial.com>
2021-01-25 21:14:01 +0100

    set up development enviroment

---
+++ template.js
@@ -0,0 +1,12 @@
+export default () => {
+  return `<!doctype html>
+    <html lang="en">
+      <head>
+        <meta charset="utf-8">
+        <title>Backend-skeleton</title>
+      </head>
+      <body>
+        <div id="root">HelloWorld!</div>
+      </body>
+    </html>`
+}
